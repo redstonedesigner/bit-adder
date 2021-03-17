24f9818cc94466d0eeebb9cfb5aee016f968c55b
redstonedesigner <redstonedesigner1@gmail.com>
2021-03-17 12:05:28 +0000

    test: Modify test cases to not use an init constructer

--- test_gates.py
+++ test_gates.py
@@ -2,23 +2,23 @@ import gatehandler
 
 
 class Tests:
-    def __init__(self):
-        self.gates = gatehandler.GateHandler()
-
     def test_nand(self):
-        assert self.gates.nand(0, 0) is True
-        assert self.gates.nand(0, 1) is True
-        assert self.gates.nand(1, 0) is True
-        assert self.gates.nand(1, 1) is False
+        gates = gatehandler.GateHandler()
+        assert gates.nand(0, 0) is True
+        assert gates.nand(0, 1) is True
+        assert gates.nand(1, 0) is True
+        assert gates.nand(1, 1) is False
 
     def test_nor(self):
-        assert self.gates.nor(0, 0) is True
-        assert self.gates.nor(0, 1) is False
-        assert self.gates.nor(1, 0) is False
-        assert self.gates.nor(1, 1) is False
+        gates = gatehandler.GateHandler()
+        assert gates.nor(0, 0) is True
+        assert gates.nor(0, 1) is False
+        assert gates.nor(1, 0) is False
+        assert gates.nor(1, 1) is False
 
     def test_xor(self):
-        assert self.gates.xor(0, 0) is False
-        assert self.gates.xor(0, 1) is True
-        assert self.gates.xor(1, 0) is True
-        assert self.gates.xor(1, 1) is False
+        gates = gatehandler.GateHandler()
+        assert gates.xor(0, 0) is False
+        assert gates.xor(0, 1) is True
+        assert gates.xor(1, 0) is True
+        assert gates.xor(1, 1) is False
