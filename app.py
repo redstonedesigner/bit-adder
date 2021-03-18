54734c4fc965fe936eb433dcf41c9e52df7d5beb
redstonedesigner <redstonedesigner1@gmail.com>
2021-03-18 10:29:11 +0000

    refactor: Move input code to util function

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
 from lib import adderhandler
-from lib.validation import BinaryNumberValidator
 import lib.utils
 
-from PyInquirer import prompt
 
+numbers = lib.utils.get_numbers()
 
-questions = [
-    {
-        "type": "input",
-        "name": "num1",
-        "message": "Enter Number 1",
-        "validate": BinaryNumberValidator
-    },
-    {
-        "type": "input",
-        "name": "num2",
-        "message": "Enter Number 2",
-        "validate": BinaryNumberValidator
-    }
-]
-
-answers = prompt.prompt(questions)
-num1 = answers['num1']
-num2 = answers['num2']
-
-while len(num1) != len(num2):
-    print("Error: The two numbers must be of the same length!")
-
-    answers = prompt.prompt(questions)
-    num1 = list(answers['num1'])
-    num2 = list(answers['num2'])
-
-num1 = lib.utils.str_to_list(num1)
-num2 = lib.utils.str_to_list(num2)
-
-num1.reverse()
-num2.reverse()
+num1 = lib.utils.str_to_list(numbers[0]).reverse()
+num2 = lib.utils.str_to_list(numbers[1]).reverse()
 
 result = []
 
--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,39 @@
+from lib.validation import BinaryNumberValidator
+from PyInquirer import prompt
+
+
 def str_to_list(string):
     value = []
     for i in string:
         value.append(int(i))
     return value
+
+
+def get_numbers():
+    questions = [
+        {
+            "type": "input",
+            "name": "num1",
+            "message": "Enter Number 1",
+            "validate": BinaryNumberValidator
+        },
+        {
+            "type": "input",
+            "name": "num2",
+            "message": "Enter Number 2",
+            "validate": BinaryNumberValidator
+        }
+    ]
+
+    answers = prompt.prompt(questions)
+    num1 = answers['num1']
+    num2 = answers['num2']
+
+    while len(num1) != len(num2):
+        print("Error: The two numbers must be of the same length!")
+
+        answers = prompt.prompt(questions)
+        num1 = list(answers['num1'])
+        num2 = list(answers['num2'])
+
+    return [num1, num2]
